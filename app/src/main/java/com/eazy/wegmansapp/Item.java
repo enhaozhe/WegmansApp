fea7acbed6ddec33505c78706e95a29b8bbf9b2b
will <williamwang3123@gmail.com>
2019-11-02 18:37:03 -0400

    add methods in Item and Recipe

--- app/src/main/java/com/eazy/wegmansapp/Item.java
+++ app/src/main/java/com/eazy/wegmansapp/Item.java
@@ -5,16 +5,28 @@ public class Item {
     public int ID;
     public String name;
     public String links;
+    public boolean isProduct;
+    public String image = "https://www.wegmans.com/content/dam/wegmans/recipes/" + "first digit of ID" + "/" + ID + ".jpg";
+
+    public Item(String name){
+        ID = -1;
+        this.name = name;
+        links = null;
+        isProduct = false;
+    }
 
     public Item(int ID, String name){
         this.ID = ID;
         this.name = name;
+        links = null;
+        isProduct = true;
     }
 
     public Item(int ID, String name, String links){
         this.ID = ID;
         this.name = name;
         this.links = links;
+        isProduct = true;
     }
 
 }
--- app/src/main/java/com/eazy/wegmansapp/Recipe.java
+++ app/src/main/java/com/eazy/wegmansapp/Recipe.java
@@ -1,10 +1,30 @@
 package com.eazy.wegmansapp;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class Recipe {
 
-    public String ID, name;
+    public int ID;
+    public String name;
+    public int timeToMake;
+    public ArrayList<Item> ingredients;
+    public String instruction;
 
-    public Recipe(){
+    public Recipe(int ID, String name){
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public void parse(String result){
+        try {
+            JSONObject resultObject = new JSONObject(result);
+        }catch(JSONException e) {
+            
+        }
 
     }
+
 }
