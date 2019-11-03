b5d67fa7585cf38964c2fbe19ed72dc50f37172b
enhaozhe <enhaozhe@buffalo.edu>
2019-11-02 20:05:39 -0400

    Search API is working rn.

--- app/src/main/java/com/eazy/wegmansapp/MainActivity.java
+++ app/src/main/java/com/eazy/wegmansapp/MainActivity.java
@@ -2,14 +2,35 @@ package com.eazy.wegmansapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String TAG = "mTAG";
+    private Wegmans_API_Search search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search = new Wegmans_API_Search("beef");
+        search.search();
     }
+
 
 }
