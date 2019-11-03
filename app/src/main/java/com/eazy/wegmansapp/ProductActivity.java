86ec6070d5c86f696fc2226efed1af12d9805835
Chaoping <chaoping@buffalo.edu>
2019-11-03 05:35:28 -0500

    Product_API_Get class

--- app/src/main/java/com/eazy/wegmansapp/ProductActivity.java
+++ app/src/main/java/com/eazy/wegmansapp/ProductActivity.java
@@ -1,6 +1,29 @@
 package com.eazy.wegmansapp;
 
-import android.app.Activity;
 
-public class ProductActivity extends Activity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductActivity extends AppCompatActivity {
+    private ImageView image;
+    private TextView description, name, consumer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+
+        Intent intent = getIntent();
+        Item item = (Item)intent.getSerializableExtra("Item");
+        Product_API_Get proget =new Product_API_Get(item);
+        proget.search();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        image=findViewById();
+}
 }
